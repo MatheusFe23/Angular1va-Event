8c5fe8758a5d100d0c618c52ac2f9095649ded3c
MatheusFe23 <155886669+MatheusFe23@users.noreply.github.com>
2025-05-04 13:55:33 -0300

    Excluir e alterar
    Git com problema, upando arquivos manualmente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
 import {AdicionarComponent} from './components/adicionar/adicionar.component';
+import {ListarComponent} from './components/listar/listar.component';
+import {ExcluirComponent} from './components/excluir/excluir.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   {path: 'adicionar', component: AdicionarComponent},
+  {path: 'listar', component: ListarComponent},
+  {path: 'excluir', component: ExcluirComponent},
 ];
